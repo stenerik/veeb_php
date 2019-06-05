ea50710944992b45acd66853a58e0d190d1efb08
dudu <sten-erik.klaos@khk.ee>
2019-06-05 12:55:38 +0300

    Muutujad

---
+++ PHP alused/variables/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristofer.vasser
+ * Date: 13.02.2019
+ * Time: 14:15
+ */
+$eesnimi = "Kristofer";
+$perenimi = "Vasser";
+$vanus = 20;
+$kaal = 61;
+
+echo "Minu eesnimi on ".$eesnimi."<br>";
+echo "Minu perekonnanimi on ".$perenimi."<br>";
+echo "Vanuseks mul ".$vanus." aastat<br>";
+echo "Kaalun ".$kaal." kg<br>";
