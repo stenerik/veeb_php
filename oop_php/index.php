d1f24f9ff24f921f4da40a73a8e709ffd4640aa0
dudu <sten-erik.klaos@khk.ee>
2019-06-05 13:02:57 +0300

    OOP Menüü

---
+++ oop_php/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'conf.php';
+// main page by html templates
+$main = new Template('app.main');
+$meta = new Template('app.meta');
+$style = new Template('app.style');
+$js = new Template('app.js');
+// add meta, style and js templates content to main template
+$main->set('meta', $meta->parse());
+$main->set('style', $style->parse());
+$main->set('js', $js->parse());
+// set up main page real values
+$main->set('lang', $http->get('lang_id'));
+$main->set('title', 'Söökla menüü');
+$mainContent = new Template('menu.main_content');
+$nav = new Template('nav.nav');
+$sql = 'SELECT * FROM dish_availability';
+$dates = $db->getData($sql);
+foreach ($dates as $date) {
+    $navItem = new  Template('nav.item');
+    $link = $http ->getLink(array('date' => $date['dish_date']));
+    $navItem->set('link', $link);
+    $navItem->set('date', $date['dish_date']);
+    $nav->add('nav_items', $navItem->parse());
+}
+$mainContent->set('nav', $nav->parse());
+$mainContent->set('header', 'Page Header');
+// page content from controller
+// add action control
+require_once 'controller.php';
+/*require_once 'nav.php';*/ // nav element
+$mainContent->set('footer', 'Page Footer');
+$main->set('content', $mainContent->parse());
+// print out main page full view
+echo $main->parse();
+$db->showHistory();
---
+++ oop_php/nav.php
@@ -0,0 +1,16 @@
+<?php
+$nav = new Template('nav.nav');
+$item = new Template('nav.item');
+$item->set('name', 'Main Page');
+$link = $http->getLink(array('controller' => 'main'));
+$item->set('link', $link);
+$nav->add('items', $item->parse());
+$item->set('name', 'Contact');
+$link = $http->getLink(array('controller' => 'contact'));
+$item->set('link', $link);
+$nav->add('items', $item->parse());
+$item->set('name', 'Log In');
+$link = $http->getLink(array('controller' => 'login'));
+$item->set('link', $link);
+$nav->add('items', $item->parse());
+$mainContent->set('nav', $nav->parse());
