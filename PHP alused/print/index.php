82840f25d178dd925bae7cf72b7824132d10b82a
dudu <sten-erik.klaos@khk.ee>
2019-06-05 12:54:47 +0300

    Foori tuled

---
+++ PHP alused/print/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="main">
+<?php
+echo '<div id="punane"></div>';
+echo '<div id="kollane"></div>';
+echo '<div id="roheline"></div>';
+?>
+        </div>
+    </body>
+</html>
+
