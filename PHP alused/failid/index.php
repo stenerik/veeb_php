6503a02cde93f351a2e71cb10b38e36b46cdcc89
dudu <sten-erik.klaos@khk.ee>
2019-06-05 12:53:09 +0300

    Raamatud

---
+++ PHP alused/failid/index.php
@@ -0,0 +1,54 @@
+<?php
+
+function failiKontroll ($failiNimi) {
+    if(is_file($failiNimi) and file_exists($failiNimi) and is_readable($failiNimi)) {
+        return true;
+    }
+    return false;
+}
+
+function loeFailist($failiNimi) {
+$raamatud = array();
+$nimetused = array(
+    'nimetus',
+    'autor',
+    'keel',
+    'lk'
+);
+    if(failiKontroll($failiNimi)) {
+        $fp = fopen($failiNimi,  'r');
+        $raamat = array();
+        while (!feof($fp)) {
+            $rida = fgets($fp);
+            if (trim($rida) == '') {
+                $raamat[] = $rida;
+            } else {
+                $raamat = array_combine($nimetused, $raamat);
+                $raamatud[] = $raamat;
+                $raamat = array();
+            }
+        }
+    }
+    return $raamatud;
+}
+
+function tabelFailist($failiNimi)  {
+    if(failiKontroll($failiNimi)) {
+        $fp = fopen($failiNimi, 'r');
+        echo '<table border="1">';
+        echo '<tr>';
+        while (!feof($fp)) {
+            $rida = fgets($fp);
+            if(trim($rida) != '') {
+                echo '<td>'.$rida. '</td>';
+            } else {
+                echo '</tr>';
+            }
+
+        }
+        echo '</table>';
+
+    }
+}
+loeFailist('raamatud.txt');
+tabelFailist('raamatud.txt');
